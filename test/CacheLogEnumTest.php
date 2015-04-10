43d8716748e84d7d6cb2867b03d2610dfbad1780
Bruno <brunobg@gmail.com>
2015-04-10 18:26:10 -0300

    CacheLogEnum test passing

--- test/CacheLogEnumTest.php
+++ test/CacheLogEnumTest.php
@@ -4,6 +4,6 @@ class CacheLogEnumTest extends PHPUnit_Framework_TestCase {
 	protected $backupGlobals = false;
 
 	public function testEnum() {
-		$this->assertGreaterThan(0, count(CacheLogEnum::getNames()));
+		$this->assertGreaterThan(0, count(Cachearium\CacheLogEnum::getNames()));
 	}
 }
