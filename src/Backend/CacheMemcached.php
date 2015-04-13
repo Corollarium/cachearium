3322ceb62565de7a80c7e5f12c8d1df09f65e545
Bruno <brunobg@gmail.com>
2015-04-13 18:14:13 -0300

    namespaces even for comments

--- src/Backend/CacheMemcached.php
+++ src/Backend/CacheMemcached.php
@@ -19,7 +19,7 @@ use Symfony\Component\Config\Definition\Exception\Exception;
 class CacheMemcached extends CacheRAM {
 	/**
 	 * The memcache var
-	 * @var Memcached
+	 * @var \Memcached
 	 */
 	private $memcached;
 
