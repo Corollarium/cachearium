0452fee1688bd012c14e9b4d3ab7c91b5d5476ba
Bruno <brunobg@gmail.com>
2015-04-13 10:11:42 -0300

    Cache.php updated

--- src/Cache.php
+++ src/Cache.php
@@ -4,10 +4,20 @@
  * Copyright (c) 2008-2014 Corollarium Tecnologia Ltda.
  */
 
+// exceptions
+require_once(__DIR__ . '/Exceptions/CacheInvalidBackendException.php');
+require_once(__DIR__ . '/Exceptions/CacheInvalidParameterException.php');
+require_once(__DIR__ . '/Exceptions/CacheUnsupportedOperation.php');
+require_once(__DIR__ . '/Exceptions/CacheInvalidDataException.php');
+require_once(__DIR__ . '/Exceptions/CacheKeyClashException.php');
+require_once(__DIR__ . '/Exceptions/NotCachedException.php');
+
+// CODE
 require_once(__DIR__ . '/CacheLogEnum.php');
 require_once(__DIR__ . '/CacheKey.php');
 require_once(__DIR__ . '/CacheException.php');
-require_once(__DIR__ . '/CacheInterface.php');
+require_once(__DIR__ . '/Cached.php');
+require_once(__DIR__ . '/CachedObject.php');
 require_once(__DIR__ . '/CacheData.php');
 require_once(__DIR__ . '/CacheAbstract.php');
 
